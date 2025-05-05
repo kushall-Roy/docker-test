712f289419483898699e3eb6d17089f5b93e63e7
kushall-Roy <roykushal1996@gmail.com>
2025-05-05 23:30:45 +0530

    demo-app-v1

---
+++ src/main/java/com/kushal/PsaDemoDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.kushal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaDemoDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaDemoDockerAppApplication.class, args);
+	}
+
+}
